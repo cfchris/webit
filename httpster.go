5cd7b5e7b35d96c3b047f9d32abc5cb8398eec72
Chris Phillips <cfchris@gmail.com>
2015-04-21 16:56:31 -0700

    Simplified directory handling.

--- httpster.go
+++ httpster.go
@@ -3,26 +3,17 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
 	"net/http"
-	"path/filepath"
 )
 
 var port string
 var path string
 
 func main() {
-	workdir, err := filepath.Abs(".")
-	if err != nil {
-		log.Fatalln("Error getting current working directory:", err.Error())
-	}
-
 	flag.StringVar(&port, "p", "3333", "Port for server to run on - defaults to 3333")
-	flag.StringVar(&path, "d", workdir, "Server directory - defaults to ./.")
+	flag.StringVar(&path, "d", "./", "Server directory - defaults to ./.")
 	flag.Parse()
 
-	httpDir := http.Dir(path)
-
-	fmt.Printf("Starting HTTPster on port %s from %s\n", port, httpDir)
-	http.ListenAndServe(":"+port, http.FileServer(httpDir))
+	fmt.Printf("Starting HTTPster on port %s from %s\n", port, path)
+	http.ListenAndServe(":"+port, http.FileServer(http.Dir(path)))
 }
