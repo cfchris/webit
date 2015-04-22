ab72a4a14db412475eb2318be5079ed992dba0db
Chris Phillips <cfchris@gmail.com>
2015-04-21 17:28:44 -0700

    Added CORS support.

--- httpster.go
+++ httpster.go
@@ -8,12 +8,27 @@ import (
 
 var port string
 var path string
+var cors bool
 
 func main() {
 	flag.StringVar(&port, "p", "3333", "Port for server to run on - defaults to 3333")
-	flag.StringVar(&path, "d", "./", "Server directory - defaults to ./.")
+	flag.StringVar(&path, "d", "./", "Server directory - defaults to ./")
+	flag.BoolVar(&cors, "c", false, "Add cors support")
 	flag.Parse()
 
+	handler := http.FileServer(http.Dir(path))
+
+	if cors {
+		handler = corsHandler(handler)
+	}
+
 	fmt.Printf("Starting HTTPster on port %s from %s\n", port, path)
-	http.ListenAndServe(":"+port, http.FileServer(http.Dir(path)))
+	http.ListenAndServe(":"+port, handler)
+}
+
+func corsHandler(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		h.ServeHTTP(w, r)
+	})
 }
